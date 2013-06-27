226c91c92330bcabc7b73f6c4e410d44c6537f63
heluyao <heluyaosj13@gmail.com>
2013-06-27 15:04:14 +0800

    6.27.0
    update

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-var jade = require('jade');
-var str = require("fs").readFileSync(__dirname +"/test.jade", "utf8");
-
-
-
+var jade = require('jade'),
+	fs = require("fs"),
+	str = require("fs").readFileSync(__dirname +"/test.jade", "utf8");
 
 jade.render(str, { filename: "test.jade" }, function(err, jade) {
     if (err) throw err;
-    var http = require('http');
+    //写入html文件
+	fs.open("index.html","w",0644,function(e,fd){
+      if(e) throw e;
+      fs.write(fd,jade,0,'utf8',function(e){
+        if(e) throw e;
+        fs.closeSync(fd);
+      });
+    });
+	var http = require('http');
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(jade);
     }).listen(8888, '127.0.0.1');
-    console.log('asaasasasddads http://127.0.0.1:8888/');
+    console.log('a http://127.0.0.1:8888/');
 });
-
-
-
-
-
